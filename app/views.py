a2092a8838613a85d4f6f36a943d71913d26b0f4
frannie <frances.ziesemer@gmail.com>
2021-02-02 16:29:25 +0100

    create login and registration forms + add validation functionality

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from .forms import RegistrationForm, LoginForm
 from app import app
-from forms import RegistrationForm, LoginForm
 
 app.config['SECRET_KEY'] = 'd9ee7cf4cfc30af058b1dd9c649c0b82'
 
@@ -22,19 +22,28 @@ posts = [
 
 @app.route('/')
 @app.route('/home')
-def index():
+def home():
     return render_template('index.html', posts=posts)
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else: 
+            flash(f'Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
